00136f2407e21bec9572de981d3f957c32170941
jinchaeeun <kbg05032@naver.com>
2021-05-31 14:47:41 +0900

    first commit spring boot

---
+++ src/main/java/com/hustar/value_coding_boot/ValueCodingBootApplication.java
@@ -0,0 +1,13 @@
+package com.hustar.value_coding_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueCodingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueCodingBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hustar/value_coding_boot/ValueCodingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hustar.value_coding_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueCodingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
