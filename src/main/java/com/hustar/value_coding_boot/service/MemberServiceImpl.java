215b9e55fe63f6c0731a7e25fd072e4fcfeda450
jinchaeeun <kbg05032@naver.com>
2021-05-31 22:49:18 +0900

    register.jsp,  회원가입 sql insert 테스트

---
+++ src/main/java/com/hustar/value_coding_boot/service/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package com.hustar.value_coding_boot.service;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.hustar.value_coding_boot.dao.MemberDAO;
+import com.hustar.value_coding_boot.vo.MemberVO;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+	
+	@Inject MemberDAO dao;
+	
+	@Override
+	public void register(MemberVO vo) throws Exception {
+		
+		dao.register(vo);
+		
+	}
+	
+}
