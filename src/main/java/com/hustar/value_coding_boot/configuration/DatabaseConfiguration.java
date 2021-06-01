bc648c00712423441bb77cfb40fde24fc6739900
jinchaeeun <kbg05032@naver.com>
2021-06-01 11:02:07 +0900

    version rollback

--- src/main/java/com/hustar/value_coding_boot/configuration/DatabaseConfiguration.java
+++ src/main/java/com/hustar/value_coding_boot/configuration/DatabaseConfiguration.java
@@ -3,7 +3,12 @@ package com.hustar.value_coding_boot.configuration;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,7 +19,10 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 @PropertySource("classpath:/application.properties") //①
 public class DatabaseConfiguration {
-
+	
+	@Autowired		//<property> 태그 명시 안해도 Dependency 객체 자동 주입 (Automatic Dependency Injection)
+    private ApplicationContext applicationContext;
+	 
     //②
     @Bean
     @ConfigurationProperties(prefix="spring.datasource.hikari")
@@ -23,10 +31,24 @@ public class DatabaseConfiguration {
     }
 
     //③
-    @Bean
+    @Bean(destroyMethod = "close")
     public DataSource dataSource() throws Exception {
         DataSource dataSource = new HikariDataSource(hikariConfig());
-        System.out.println(dataSource.toString());
+//        System.out.println(dataSource.toString());
         return dataSource;
     }
+    
+    @Bean			//MyBatis에서 Sql Session 생성한다. SqlSessionFactory는 데이터베이스 연결과 SQL 실행에 대한 모든 것을 한다. SqlSessionFactory를 생성해주는 객체는 SqlSessionFactoryBean이다.
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/*.xml"));	//MyBatis Mapper 파일(sql 쿼리) 위치를 설정, **은 하위 폴더, *.xml은 xml 파일 전체를 의미
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean			//SqlSession을 구현하고 코드에서 SqlSession을 대체하는 역할, 여러 개의 DAO나 Mapper에서 공유할 수 있다.
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+    
 }
--- src/main/java/com/hustar/value_coding_boot/controller/MemberController.java
+++ src/main/java/com/hustar/value_coding_boot/controller/MemberController.java
@@ -3,12 +3,40 @@ package com.hustar.value_coding_boot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.RequestMethod; 
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hustar.value_coding_boot.service.MemberService;
+import com.hustar.value_coding_boot.vo.MemberVO;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller 
 @RequestMapping("/member/**")
 public class MemberController { 
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	@Inject
+	MemberService service;
+	
+	// 회원가입 get
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void getRegister() throws Exception {
+		logger.info("get register");
+	}
+	
+	// 회원가입 post
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String postRegister(MemberVO vo) throws Exception {
+		logger.info("post register");
+		
+		service.register(vo);
+		
+		return null;
+	}
+	
 	@GetMapping("login") 
 	public String member_login() { 
 		
