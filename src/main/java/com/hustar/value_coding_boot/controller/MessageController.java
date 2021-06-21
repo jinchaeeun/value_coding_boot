4fa427094f267c42e8137d3fa1d7e0b9f7bb47b2
Hyunstagram <fgrt1006@naver.com>
2021-06-21 20:27:56 +0900

    알림 세션 단일화

--- src/main/java/com/hustar/value_coding_boot/controller/MessageController.java
+++ src/main/java/com/hustar/value_coding_boot/controller/MessageController.java
@@ -2,45 +2,25 @@ package com.hustar.value_coding_boot.controller;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @ServerEndpoint("/websocket")
 public class MessageController extends Socket {
-    private static final List<Session> session = new ArrayList<Session>();
-
-    @OnOpen
-    public void open(Session newUser) {
-        System.out.println("connected");
-        session.add(newUser);
-        System.out.println(newUser.getId());
-    }
-
+	
     @OnMessage
-    public void getMsg(Session recieveSession, String msg) {
-        for (int i = 0; i < session.size(); i++) {
-            if (!recieveSession.getId().equals(session.get(i).getId())) {
-                try {
-                    session.get(i).getBasicRemote().sendText(msg);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }else{
-                try {
-                    session.get(i).getBasicRemote().sendText(msg);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    public void getMsg(Session session, String msg) {
+        try {
+            session.getBasicRemote().sendText(msg);
+        } catch (IOException e) {
+
+            System.out.println("hey");
+            e.printStackTrace();
         }
     }
 }
