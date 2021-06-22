18c60097cbed20c20f5316d509b68311f4d23fab
Hyunstagram <fgrt1006@naver.com>
2021-06-22 16:15:38 +0900

    답글 오류 수정 | 답글 시 알림 인서트

--- src/main/java/com/hustar/value_coding_boot/controller/AnswerController.java
+++ src/main/java/com/hustar/value_coding_boot/controller/AnswerController.java
@@ -41,7 +41,7 @@ public class AnswerController {
 		
 		// request로 값을 받아와서 answerVO에 저장
 		answerVO.setAns_contents(request.getParameter("ans_contents"));
-		answerVO.setPo_num(Integer.parseInt(request.getParameter("po_num")));
+		answerVO.setAns_po_num(Integer.parseInt(request.getParameter("po_num")));
 		answerVO.setAns_writer(request.getParameter("ans_writer"));
 		
 		answerService.write(answerVO);
@@ -55,6 +55,7 @@ public class AnswerController {
 		boardService.updateAnsCnt(Integer.parseInt(request.getParameter("po_num")));
 		
 		BoardVO boardVO = boardService.read(Integer.parseInt(request.getParameter("po_num")));
+		System.out.println(boardVO.getPo_title());
 	
 		Course course = new Course();
 		course.setNoti_message("'" + boardVO.getPo_title() + "' 질문의 답변이 등록되었습니다.");
@@ -73,7 +74,7 @@ public class AnswerController {
 		
 		// request로 값을 받아와서 answerVO에 저장
 		answerVO.setAns_contents(request.getParameter("update-answer-contents"));
-		answerVO.setPo_num(Integer.parseInt(request.getParameter("po_num")));
+		answerVO.setAns_po_num(Integer.parseInt(request.getParameter("po_num")));
 		answerVO.setAns_num(Integer.parseInt(request.getParameter("ans_num")));
 		
 		answerService.update(answerVO);
@@ -90,7 +91,7 @@ public class AnswerController {
 		AnswerVO answerVO = new AnswerVO();
 		
 		answerVO.setAns_num(ans_num);
-		answerVO.setPo_num(po_num);
+		answerVO.setAns_po_num(po_num);
 		
 		AnswerVO parentVO = answerService.detail(answerVO);
 		
@@ -119,15 +120,15 @@ public class AnswerController {
 		
 		re_answerVO.setAns_group_num(answerVO.getAns_num());
 		re_answerVO.setAns_contents(answerVO.getAns_contents());
-		re_answerVO.setPo_num(answerVO.getPo_num());
+		re_answerVO.setAns_po_num(answerVO.getAns_po_num());
 		re_answerVO.setAns_writer(answerVO.getAns_writer());
 		re_answerVO.setAns_depth(1);
 		re_answerVO.setAns_group_order(group_order + 1);
 		
 		// 대댓글 입력
 		answerService.childInsert(re_answerVO);
-		boardService.updateAnsCnt(answerVO.getPo_num());
+		boardService.updateAnsCnt(answerVO.getAns_po_num());
 		
-		return "redirect:/board/notice_view?po_num=" + answerVO.getPo_num();
+		return "redirect:/board/notice_view?po_num=" + answerVO.getAns_po_num();
 	}
 }
